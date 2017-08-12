86b2b2e24ece2cd279bc3cfdd524fde029153006
ganesan049 <30718676+ganesan049@users.noreply.github.com>
2017-08-12 18:31:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,18 @@ button.onclick=function(){
   var span = document.getElementById('count');
   span.innerHTML=counter.toString();
 };
+...button.onclick=function(){
+       var  request = new XMLHttpRequest();
+       request.onsteadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+       } 
+      };
+    request.open('GET','http://ganesanece49.imad.hasura-app.io/counter',true);
+    request.send(null);
+};...
